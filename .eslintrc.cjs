7001ab8884e36e3277e25886d4456062e82e2374
Aurelien JEANNEAU <aurelienjeanneau@gmail.com>
2023-06-13 17:13:01 +0200

    setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,12 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    "indent": [ "error", 2 ],
+    "vue/html-indent": ["error"]
   }
 }
