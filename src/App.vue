1aa0fed1937a000c7d2ab9a43b8fd202a31ab20c
Aurelien JEANNEAU <aurelienjeanneau@gmail.com>
2023-06-28 14:15:02 +0200

    projects fetch at startup

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,32 @@
         <RouterView />
     </div>
 </template>
+<script setup lang="ts">
+import { useProjectStore } from "./stores/projects";
+import { type RawProject, type Project } from "./typing/project";
+
+const projectStore = useProjectStore();
+
+fetch("http://192.168.14.30:8080/projects/", {
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+    .then((projects) => {
+        return projects.json();
+    })
+    .then((rawProjects: RawProject[]) => {
+        projectStore.projects = rawProjects.map<Project>((rawProject) => {
+            return {
+                id: rawProject.id,
+                code: rawProject.project_code,
+                division: rawProject.division,
+                name: rawProject.project_name,
+                sub_category: rawProject.sub_category,
+                classification: rawProject.classification,
+                expansion_renewal: rawProject.expansion_renewal,
+                manager: rawProject.project_manager,
+            };
+        });
+    });
+</script>
