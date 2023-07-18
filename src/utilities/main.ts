7ab38267ccb307c912bd88d17117cc8ad82443eb
Aurelien JEANNEAU <aurelienjeanneau@gmail.com>
2023-07-18 10:30:01 +0200

    comments

--- src/utilities/main.ts
+++ src/utilities/main.ts
@@ -2,6 +2,11 @@ const now = new Date();
 import type { days } from "@/typing";
 import { parse, format } from "date-fns";
 
+/**
+ * Parse weekNumber, year to human readable date
+ * @param weekNumber week Number (starting at one)
+ * @param year year Number
+ */
 export const weekNumberToString = (weekNumber: number, year: number) => {
     const firstDay: Date = parse(`${year}-${weekNumber}-Monday`, "Y-w-EEEE", now);
     const lastDay: Date = parse(`${year}-${weekNumber}-Friday`, "Y-w-EEEE", now);
@@ -25,11 +30,23 @@ export const dayValidation = (day: string | string[]): days | null => {
     return dayNb === 0 || dayNb === 1 || dayNb === 2 || dayNb === 3 || dayNb === 4 ? dayNb : null;
 };
 
+/**
+ * Parse day, weekNumber, year to format YYYY-MM-DD
+ * @param day day number (0: Monday, 1, Tuesday...)
+ * @param weekNumber week Number (starting at one)
+ * @param year year Number
+ */
 export const dayNumberToDayDate = (day: days, weekNumber: number, year: number) => {
     const dayDate: Date = parse(`${year}-${weekNumber}-${((day + 1) % 7) + 1}`, "Y-w-e", now);
     return format(dayDate, "yyyy-MM-dd");
 };
 
+/**
+ * Parse day weekNumber, year to human readable date
+ * @param day day number (0: Monday, 1, Tuesday...)
+ * @param weekNumber week Number (starting at one)
+ * @param year year Number
+ */
 export const dayNumberToString = (day: days, weekNumber: number, year: number) => {
     const dayDate: Date = parse(`${year}-${weekNumber}-${day + 1}`, "Y-w-e", now);
     return format(dayDate, "MMMM d, yyyy");
@@ -41,6 +58,11 @@ export function assertIsNode(e: EventTarget | null): asserts e is Node {
     }
 }
 
+/**
+ * Function range as the python range function
+ * @param begining strating of the array
+ * @param end end of the array, (excluding last number)
+ */
 export function range(begining: number, end: number): number[] {
     if (begining >= end) {
         return [];
@@ -54,6 +76,10 @@ export function range(begining: number, end: number): number[] {
     return range;
 }
 
+/**
+ * It sends an error when Environnement variable is missing
+ * @param envVariableKey Key f the environnement variable
+ */
 export function envVariableWithValidation(envVariableKey: string) {
     const envVariable = import.meta.env[envVariableKey];
     if (typeof envVariable !== "string") {
