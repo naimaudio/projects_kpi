5a73d56f47b6eb323c5c5b2aecdea7a63bc168a8
Aurelien JEANNEAU <aurelienjeanneau@gmail.com>
2023-08-22 16:58:13 +0200

    error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,36 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import fs from "fs";
-import { envVariableWithValidation } from "./src/utilities/main";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: "/app",
-    plugins: [
-        vue({
-            template: {
-                compilerOptions: {
-                    isCustomElement: (tag) => {
-                        return tag.startsWith("fluent");
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        base: "/app",
+        plugins: [
+            vue({
+                template: {
+                    compilerOptions: {
+                        isCustomElement: (tag) => {
+                            return tag.startsWith("fluent");
+                        },
                     },
                 },
+            }),
+        ],
+        server: {
+            https: {
+                key: fs.readFileSync(env.SSL_KEY_PATH),
+                cert: fs.readFileSync(env.SSL_CERT_PATH),
+                passphrase: env.SSL_KEY_PASSPHRASE,
             },
-        }),
-    ],
-    server: {
-        https: {
-            key: fs.readFileSync(envVariableWithValidation("VITE_SSL_KEY_PATH")),
-            cert: fs.readFileSync(envVariableWithValidation("VITE_SSL_CERT_PATH")),
-            passphrase: envVariableWithValidation("VITE_SSL_KEY_PASSPHRASE"),
         },
-    },
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
         },
-    },
+    };
 });
